abd6173fd37f0f778978d85c40ba699415b2c7f7
young-guoxiangguo <62686445+young-guoxiangguo@users.noreply.github.com>
2020-03-29 15:30:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,4 +6,6 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/10.10.1.205/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.13.203/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/net203/g' package/base-files/files/bin/config_generate
+sed -i "s/set network\.\$1\.proto='static'/set network.\$1.proto='static'\nset network.\$1.gateway='10.10.13.12'\nset network.\$1.dns='114.114.114.114 223.5.5.5'\n/g" package/base-files/files/bin/config_generate
